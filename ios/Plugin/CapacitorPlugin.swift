27142ce880b3879ac4d222524fd3ecad41024dc4
Wolfgang Mathurin <wmathurin@salesforce.com>
2023-07-13 17:51:45 -0600

    SDKInfoPlugin for iOS (partially implemented)

--- ios/Plugin/CapacitorPlugin.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@objc public class CapacitorPlugin: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
-    }
-}
---
+++ ios/Plugin/SDKInfoPlugin.swift
@@ -0,0 +1,62 @@
+import Foundation
+import Capacitor
+import SalesforceSDKCore
+
+@objc (SDKInfoPlugin)
+public class SDKInfoPlugin: CAPPlugin {
+    
+    @objc func getInfo(_ call: CAPPluginCall) {
+        call.resolve(SDKInfo.getSDKInfo().dictionary)
+    }
+    
+    @objc func registerAppFeature(_ call: CAPPluginCall) {
+        let appFeatureCode = call.getString("feature") ?? ""
+        if (!appFeatureCode.isEmpty) {
+            SFSDKAppFeatureMarkers.registerAppFeature(appFeatureCode)
+        }
+        call.resolve()
+    }
+
+    @objc func unregisterAppFeature(_ call: CAPPluginCall) {
+        let appFeatureCode = call.getString("feature") ?? ""
+        if (!appFeatureCode.isEmpty) {
+            SFSDKAppFeatureMarkers.unregisterAppFeature(appFeatureCode)
+        }
+        call.resolve()
+    }
+}
+
+struct SDKInfo {
+    let sdkVersion: String
+    let appName:String
+    let appVersion: String
+    let forcePluginsAvailable: [String]
+    let bootConfig: String
+    
+    var dictionary: [String: Any] {
+        return [
+            "sdkVersion": sdkVersion,
+            "appName": appName,
+            "appVersion": appVersion,
+            "forcePluginsAvailable": forcePluginsAvailable,
+            "bootConfig": bootConfig
+        ]
+    }
+    
+    static func getSDKInfo() -> SDKInfo {
+        let appName = getBundleValue(kCFBundleNameKey as String)
+        let appVersion = getBundleValue(kCFBundleVersionKey as String)
+        
+        return SDKInfo(
+            sdkVersion: "", // fixme
+            appName: appName,
+            appVersion: appVersion,
+            forcePluginsAvailable: [], // fixme
+            bootConfig: "" // fixme
+        )
+    }
+    
+    static func getBundleValue(_ key: String) -> String {
+        return Bundle.main.infoDictionary?[key] as? String ?? ""
+    }
+}
